469b673936357cefdae075941eafdc128a928fa3
Hide <padocon@naver.com>
2020-01-11 16:05:08 +0900

    Add test frame

---
+++ codelog_back/tests/usecases/test_feed_usecase.py
@@ -0,0 +1,18 @@
+def test_get_feed_list_usecase():
+    pass
+
+
+def test_create_feed_usecase():
+    pass
+
+
+def test_get_tag_list_usecase():
+    pass
+
+
+def test_search_feed_usecase():
+    pass
+
+
+def test_delete_feed_usecase():
+    pass
